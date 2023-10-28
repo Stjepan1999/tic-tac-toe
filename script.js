d33a79d486ee43f9f6f50f4a7ea274644a4d2366
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-10-28 09:09:14 +0200

    Add logic to check if game is over

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gameboard = (function(){
     const board = ['', '', '', '', '', '', '', '', '']
 
+    const getBoard = () => {return board}
 
     const showBoard = () => {
         for (let i = 0; i < board.length; i++) {
@@ -23,7 +24,7 @@ const gameboard = (function(){
         }
     }
 
-    return {showBoard, resetBoard, makeMove}
+    return {showBoard, resetBoard, makeMove, getBoard}
 })()
 
 
@@ -54,13 +55,41 @@ function gameController() {
         board.makeMove(index, getActivePlayer().playerSymbol);
         board.showBoard()
 
+        checkWinner()
         switchPlayers();
         printNewRound();
 
     }
 
+    const checkWinner = () => {
+        const winningCombinations = [
+            [0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]
+        ];
+
+        let isWinner;
+        for (const combination of winningCombinations) {
+            isWinner = true;
+            for (const position of combination) {  
+                //If there is no winner at each combination, 
+                //exit the loop and check for other combination            
+                if (board.getBoard()[position] !== getActivePlayer().playerSymbol) {
+                    isWinner = false;
+                    break
+                }
+            }
+            
+            if (isWinner) {
+                console.log("Winner is: ", getActivePlayer().playerName)
+                break
+            }
+        }
+        //Checking if it is tie, if no empty space and no winner it is tie
+        if (!board.getBoard().includes('') && !isWinner) {
+            console.log("It is tie")
+        }
+    }
+
     const printNewRound = () => {
-        board.showBoard();
         console.log(`${getActivePlayer().playerName}'s turn.`)
     }
 
